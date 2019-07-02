95ea0703c5ed97a6b5a0e1625e3151b438c2b3e9
gameming <chakron55@gmail.com>
2019-07-02 16:45:04 +0700

    register wtf

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,4 +1,3 @@
-import 'package:async/async.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -7,85 +6,129 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
-//Explicit
+  // Explicit
+  final formKey = GlobalKey<FormState>();
+  String nameString, emailString, passwordString;
 
-//Method
-  Widget nameTecxt() {
+  // Method
+  Widget nameText() {
     return TextFormField(
       decoration: InputDecoration(
         labelText: 'Name :',
-        labelStyle: TextStyle(color: Colors.pink),
+        labelStyle: TextStyle(color: Colors.pink[400]),
         helperText: 'Type Display Name',
-        helperStyle: TextStyle(color: Colors.pink),
+        helperStyle: TextStyle(color: Colors.pink[400]),
         icon: Icon(
-          Icons.face, 
+          Icons.face,
           size: 36.0,
-          color: Colors.pink[400],),
+          color: Colors.pink[400],
         ),
-    );
-  }
-  Widget emailTecxt() {
-    return TextFormField(
-      decoration: InputDecoration(
-        labelText: 'Email :',
-        labelStyle: TextStyle(color: Colors.pink),
-        helperText: 'Type Display Email',
-        helperStyle: TextStyle(color: Colors.pink),
-        icon: Icon(
-          Icons.email, 
-          size: 36.0,
-          color: Colors.pink[400],),
-        ),
-    );
-  }
-  Widget passwordTecxt() {
-    return TextFormField(
-      decoration: InputDecoration(
-        labelText: ' password:',
-        labelStyle: TextStyle(color: Colors.pink),
-        helperText: 'Type Display TELL',
-        helperStyle: TextStyle(color: Colors.pink),
-        icon: Icon(
-          Icons.lock, 
-          size: 36.0,
-          color: Colors.red[400],),
-        ),
+      ),validator: (String value){
+       if (value.isEmpty){
+         return'Please Fill Neme in Bank';
+
+      }
+    },onSaved: (String value){
+        nameString = value;
+
+    },
     );
   }
 
-  Widget uploadBotton() {
+  Widget emailText() {
+    return TextFormField(keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email :',
+        labelStyle: TextStyle(color: Colors.blue),
+        helperText: 'Type you@email.com',
+        helperStyle: TextStyle(color: Colors.blue),
+        icon: Icon(
+          Icons.email,
+          size: 36.0,
+          color: Colors.blue,
+        ),
+      ),validator: (String value){
+        if (((value.contains('@'))&&(value.contains('.')))) {
+          return 'Type Email Format';
+        
+
+
+        }
+      },
+    );
+  }
+
+  Widget passwordText() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Password :',
+        labelStyle: TextStyle(color: Colors.green),
+        helperText: 'More 6 Charactor',
+        helperStyle: TextStyle(color: Colors.green),
+        icon: Icon(
+          Icons.lock,
+          size: 36.0,
+          color: Colors.green,
+        ),
+      ),validator: (String value){
+         if(value.length <= 5){
+          return 'pass Much more 6 Charactor';
+
+        }
+      },onSaved: (String value){
+        if (value.length <= 5) {
+          passwordString = value;
+        }
+      },
+    );
+  }
+
+  Widget uploadButton() {
     return IconButton(
       icon: Icon(Icons.cloud_upload),
-      onPressed: () {},
+      onPressed: () {
+        print('Upload');
+        if (formKey.currentState.validate()) {
+          formKey.currentState.save();
+          print('Name = $nameString, Email =$emailString, pass = $passwordString');
+
+        }
+      },
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold(resizeToAvoidBottomPadding: false,
       appBar: AppBar(
-        backgroundColor: Colors.yellow,
+        backgroundColor: Colors.green[800],
         title: Text('Register'),
-        actions: <Widget>[uploadBotton()],
+        actions: <Widget>[uploadButton()],
       ),
-      body: Container(decoration: BoxDecoration(
-        image: DecorationImage(image: AssetImage('images/game.png'),fit: BoxFit.cover),
-      ),
+      body: Form(key: formKey,
+        child:  Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('images/game.png'), fit: BoxFit.cover),
+        ),
         alignment: Alignment.topCenter,
         padding: EdgeInsets.only(top: 60.0),
         child: Container(
-          decoration: BoxDecoration(color: Color.fromRGBO(255,255,255,0.7)),
+          padding: EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(255, 255, 255, 0.8),
+          ),
           width: 300.0,
           height: 300.0,
           child: Column(
             children: <Widget>[
-              nameTecxt(),
-              emailTecxt(),
-              passwordTecxt(),
+              nameText(),
+              emailText(),
+              passwordText(),
             ],
           ),
         ),
-      ),
+      ),),
     );
   }
 }
